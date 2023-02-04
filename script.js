11d15c8ac44ccbacdfcedaea1c60975279840785
Your Name <you@example.com>
2023-02-05 01:03:11 +0900

    add account number

--- script.js
+++ script.js
@@ -132,3 +132,18 @@ function copyAccountFive() {
   // Alert the copied text
   alert("복사되었습니다. " + copyText.value);
 }
+
+function copyAccountSix() {
+  // Get the text field
+  var copyText = document.getElementById("accountInput6");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+  
+  // Alert the copied text
+  alert("복사되었습니다. " + copyText.value);
+}
